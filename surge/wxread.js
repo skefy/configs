884f2631203d1b14643bb4341ad317821f199223
Leon <pengliang.guo1125@gmail.com>
2019-10-28 17:22:56 +0800

    try fix wxread

--- surge/wxread.js
+++ surge/wxread.js
@@ -1,16 +1,36 @@
 var body = $response.body;
-var url = $request.url;
-const path = "/pay/memberCardSummary";
-let obj = JSON.parse(body);
-if (url.indexOf(path) != -1) {
-        obj["expired"] = 0;
-	obj["expiredTime"] = 1591804799;
-	obj["remainTime"] = 86313600;
-	body = JSON.stringify(obj);
- }
-$done({body});
+const path1 = "/pay/memberCardSummary";
+const path2 = "/pay/balance";
+var path = $request.path;
+function modify_card() {
+  let obj = JSON.parse(body);
+  obj["remainCoupon"] = 99999;
+  obj["expiredTime"] = 1594223999;
+  obj["expired"] = 0;
+  obj["remainTime"] = 1330440;
+  body = JSON.stringify(obj);
+}
 
+function modify_balance() {
+  let obj = JSON.parse(body);
+  obj["balance"] = 200;
+  obj["giftBalance"] = 100;
+  obj["peerBalance"] = 100;
+  body = JSON.stringify(obj);
+}
+   
+if (path.indexOf(path1) != -1){
+  modify_card();
+}
+if (path.indexOf(path2) != -1 ){
+  modify_balance();
+}
+$done(body);
 
-//by HoGer
-//hostname = i.weread.qq.com
-//http-response https?:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary
+/**********************************************************
+微信读书 Forked from yxiaocai
+Surge
+[Script]
+http-response ^https?:\/\/i.weread.qq.com\/pay script-path=https://raw.githubusercontent.com/JO2EY/Rules/master/Script/iweread.js,requires-body=true
+[MITM]
+hostname = i.weread.qq.com
